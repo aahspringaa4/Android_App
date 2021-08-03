4916f385e4c48a716bb9437c890747e5349db2ae
aahspringaa4 <aahspringaa4@naver.com>
2021-08-03 20:54:57 +0900

    [ADD] 대마고 급식 조회 앱

---
+++ MealProject/app/src/main/java/com/example/mealproject/MealResponse.kt
@@ -0,0 +1,4 @@
+package com.example.mealproject
+
+data class MealResponse(val breakfast : List<String>, val lunch : List<String>, val dinner : List<String>)
+//data class MealResponse(val breakfast:List<String>, val lunch:List<String>, val dinner:List<String>)
