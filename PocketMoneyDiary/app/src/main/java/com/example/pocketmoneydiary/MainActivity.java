11bd0f26248b0a9405532fab6c79842b1bf807f5
aahspringaa4 <aahspringaa4@naver.com>
2021-08-23 23:20:00 +0900

    [Feat] MainActivity 수정

--- PocketMoneyDiary/app/src/main/java/com/example/pocketmoneydiary/MainActivity.java
+++ PocketMoneyDiary/app/src/main/java/com/example/pocketmoneydiary/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.pocketmoneydiary;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -24,5 +26,13 @@ public class MainActivity extends AppCompatActivity {
         money.bringToFront();
         pocket.bringToFront();
         save.bringToFront();
+
+        bt_scan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ScanActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
