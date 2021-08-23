d76e7c63603da04cd8a014eef2b512ccab90432b
aahspringaa4 <aahspringaa4@naver.com>
2021-08-23 22:27:14 +0900

    [Feat] MainActivity 수정

--- PocketMoneyDiary/app/src/main/java/com/example/pocketmoneydiary/MainActivity.java
+++ PocketMoneyDiary/app/src/main/java/com/example/pocketmoneydiary/MainActivity.java
@@ -4,16 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText money, pocket, save;
+    EditText pocket, save;
+    TextView money;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        money = (EditText)findViewById(R.id.money);
+        money = (TextView)findViewById(R.id.money);
         pocket = (EditText)findViewById(R.id.pocket);
         save = (EditText)findViewById(R.id.save);
         money.bringToFront();
