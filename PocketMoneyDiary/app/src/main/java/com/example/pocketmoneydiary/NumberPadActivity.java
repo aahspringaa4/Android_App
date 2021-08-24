83faf1fb9ed159d1d4c6cee9376eb555b275ae5f
aahspringaa4 <aahspringaa4@naver.com>
2021-08-24 19:47:01 +0900

    [Feat] 금액 입력 용 액티비티 생성

---
+++ PocketMoneyDiary/app/src/main/java/com/example/pocketmoneydiary/NumberPadActivity.java
@@ -0,0 +1,14 @@
+package com.example.pocketmoneydiary;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NumberPadActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_number_pad);
+    }
+}
