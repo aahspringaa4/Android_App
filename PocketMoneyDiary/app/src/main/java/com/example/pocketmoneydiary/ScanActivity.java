13baecd83541f1f8010f00154af19d6ef06fc03e
aahspringaa4 <aahspringaa4@naver.com>
2021-08-24 14:30:38 +0900

    [Fix] 코드 수정

--- PocketMoneyDiary/app/src/main/java/com/example/pocketmoneydiary/ScanActivity.java
+++ PocketMoneyDiary/app/src/main/java/com/example/pocketmoneydiary/ScanActivity.java
@@ -4,22 +4,70 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class ScanActivity extends AppCompatActivity {
 
     ImageButton back;
+    Spinner sp_year, sp_month, sp_day;
+    TextView year, month, day;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scan);
 
         back = (ImageButton)findViewById(R.id.back);
+        sp_year = (Spinner)findViewById(R.id.sp_year);
+        sp_month = (Spinner)findViewById(R.id.sp_month);
+        sp_day = (Spinner) findViewById(R.id.et_day);
+        year = (TextView)findViewById(R.id.year);
+        month = (TextView) findViewById(R.id.month);
+        day = (TextView)findViewById(R.id.day);
 
         back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 finish();
+            }
+        });
+
+        sp_year.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                year.setText(parent.getItemAtPosition(position).toString());
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+        sp_month.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                month.setText(parent.getItemAtPosition(position).toString());
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+
+        sp_day.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                day.setText(parent.getItemAtPosition(position).toString());
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
             }
         });
     }
