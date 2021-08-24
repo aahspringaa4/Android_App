e4c7f4ab81cef88884959c2a506de7d3060146df
aahspringaa4 <aahspringaa4@naver.com>
2021-08-24 17:27:09 +0900

    [Feat] 날짜 입력 dialog 추가

--- PocketMoneyDiary/app/src/main/java/com/example/pocketmoneydiary/ScanActivity.java
+++ PocketMoneyDiary/app/src/main/java/com/example/pocketmoneydiary/ScanActivity.java
@@ -6,6 +6,7 @@ import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageButton;
@@ -21,8 +22,8 @@ public class ScanActivity extends AppCompatActivity {
 
     private static TextView year;
     ImageButton back;
-    Spinner sp_year, sp_month, sp_day;
-    public TextView month, day;
+    Button nowaday;
+    private static TextView month, day;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,13 +31,10 @@ public class ScanActivity extends AppCompatActivity {
         setContentView(R.layout.activity_scan);
 
         back = (ImageButton) findViewById(R.id.back);
-        sp_year = (Spinner) findViewById(R.id.sp_year);
-        sp_month = (Spinner) findViewById(R.id.sp_month);
-        sp_day = (Spinner) findViewById(R.id.et_day);
         year = (TextView) findViewById(R.id.year);
         month = (TextView) findViewById(R.id.month);
         day = (TextView) findViewById(R.id.day);
-
+        nowaday = (Button)findViewById(R.id.nowaday);
         back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -44,58 +42,29 @@ public class ScanActivity extends AppCompatActivity {
             }
         });
 
-        sp_year.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+        nowaday.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                year.setText(parent.getItemAtPosition(position).toString());
-            }
+            public void onClick(View v) {
+                Calendar c = Calendar.getInstance();
+                int nYear = c.get(Calendar.YEAR);
+                int nMon = c.get(Calendar.MONTH);
+                int nDay = c.get(Calendar.DAY_OF_MONTH);
 
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-            }
-        });
-        sp_month.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                month.setText(parent.getItemAtPosition(position).toString());
-            }
+                DatePickerDialog.OnDateSetListener mDateSetListener =
+                        new DatePickerDialog.OnDateSetListener() {
+                            public void onDateSet(DatePicker view, int year, int monthOfYear,
+                                                  int dayOfMonth) {
+                                ScanActivity.year.setText(String.valueOf(year) + "년");
+                                ScanActivity.month.setText(String.valueOf(monthOfYear + 1)+ "월");
+                                ScanActivity.day.setText(String.valueOf(dayOfMonth)+ "일");
+                            }
+                        };
 
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-            }
-        });
-
-        sp_day.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                day.setText(parent.getItemAtPosition(position).toString());
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-
+                DatePickerDialog oDialog = new DatePickerDialog(ScanActivity.this,
+                        android.R.style.Theme_DeviceDefault_Light_Dialog,
+                        mDateSetListener, nYear, nMon, nDay);
+                oDialog.show();
             }
         });
-        Calendar c = Calendar.getInstance();
-        int nYear = c.get(Calendar.YEAR);
-        int nMon = c.get(Calendar.MONTH);
-        int nDay = c.get(Calendar.DAY_OF_MONTH);
-
-        DatePickerDialog.OnDateSetListener mDateSetListener =
-                new DatePickerDialog.OnDateSetListener() {
-                    public void onDateSet(DatePicker view, int year, int monthOfYear,
-                                          int dayOfMonth) {
-                        ScanActivity.year.setText(String.valueOf(year) + "년 ");
-                        ScanActivity.year.setText(ScanActivity.year.getText() + String.valueOf(monthOfYear + 1)+ "월 ");
-                        ScanActivity.year.setText(ScanActivity.year.getText() + String.valueOf(dayOfMonth + 1)+ "일");
-
-                        Toast.makeText(getApplicationContext(), year, Toast.LENGTH_SHORT).show();
-                    }
-                };
-
-        DatePickerDialog oDialog = new DatePickerDialog(this,
-                android.R.style.Theme_DeviceDefault_Light_Dialog,
-                mDateSetListener, nYear, nMon, nDay);
-        oDialog.show();
     }
 }
