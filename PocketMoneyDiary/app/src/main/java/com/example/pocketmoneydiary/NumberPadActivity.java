480e2eb4b900a73d4d9c6f9d15e6e5a52321e861
aahspringaa4 <aahspringaa4@naver.com>
2021-08-24 22:14:55 +0900

    [Feat] 넘버 패드 엑티비티 기능 추가

--- PocketMoneyDiary/app/src/main/java/com/example/pocketmoneydiary/NumberPadActivity.java
+++ PocketMoneyDiary/app/src/main/java/com/example/pocketmoneydiary/NumberPadActivity.java
@@ -3,21 +3,14 @@ package com.example.pocketmoneydiary;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 
 public class NumberPadActivity extends AppCompatActivity {
 
     EditText et_money;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_number_pad);
-
-        InputMethodManager imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
-        et_money = (EditText) findViewById(R.id.et_money);
-
-        imm.hideSoftInputFromWindow(et_money.getWindowToken(), 0);
     }
 }
