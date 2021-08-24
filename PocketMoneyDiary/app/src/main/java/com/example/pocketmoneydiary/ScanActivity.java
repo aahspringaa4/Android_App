26f311b03808d574f533237ec972c8298b226e0e
aahspringaa4 <aahspringaa4@naver.com>
2021-08-24 13:38:25 +0900

    [Feat] 기능 추가

--- PocketMoneyDiary/app/src/main/java/com/example/pocketmoneydiary/ScanActivity.java
+++ PocketMoneyDiary/app/src/main/java/com/example/pocketmoneydiary/ScanActivity.java
@@ -3,12 +3,24 @@ package com.example.pocketmoneydiary;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class ScanActivity extends AppCompatActivity {
 
+    ImageButton back;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scan);
+
+        back = (ImageButton)findViewById(R.id.back);
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
